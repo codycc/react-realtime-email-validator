35284b2b7ab59c2a657ba20bc0350d794c056793
Cody Condon <codycondon50@gmail.com>
2016-02-27 14:37:34 -0500

    add Name Field component, and clear out value on submit

--- src/components/EmailField.jsx
+++ src/components/EmailField.jsx
@@ -3,7 +3,7 @@ var validator = require('email-validator');
 
 var EmailField = React.createClass({
   getInitialState: function() {
-    return {valid: true, value: ""}
+    return {valid: true, value: ""};
   },
   onChange: function(e) {
     var val = e.target.value;
@@ -12,11 +12,13 @@ var EmailField = React.createClass({
       this.setState({valid: false, value: e.target.value})
     }else {
     this.setState({valid: true, value: e.target.value});
-  }
+    }
+  },
+  clear: function() {
+    this.setState({valid: true, value: ""})
 
   },
   render: function () {
-
     var formClass =  this.state.valid? "form-group" : "form-group has-error"
     return (
       <div className = {formClass}>
